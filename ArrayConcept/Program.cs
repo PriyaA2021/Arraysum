672d76495189c7027fde29313928e1fec38302bf
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-18 10:30:47 +1200

    Add project files.

---
+++ ArrayConcept/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArrayConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a=new int [6];
+            int sum = 0;
+            Console.WriteLine("Enter values for the array");
+            
+            for (int i = 0; i < 5; i++)
+            {
+               a[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                
+                sum = sum + a[j];
+                
+            }
+            Console.WriteLine(sum);
+
+
+
+        }
+    }
+}
